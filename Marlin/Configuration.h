6b497500bf4d3974144ee623da73b8a143fbc46d
NicholasR <nicholasr@hotmail.de>
2024-06-01 02:11:54 +0200

    Max temperatures raised to 350°C nozzle and 135°C bed. Caution: Only use temperatures your hotend and thermistor are capable of !

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -558,7 +558,7 @@
  *   998 : Dummy Table that ALWAYS reads 25°C or the temperature defined below.
  *   999 : Dummy Table that ALWAYS reads 100°C or the temperature defined below.
  */
-#define TEMP_SENSOR_0 1
+#define TEMP_SENSOR_0 61
 #define TEMP_SENSOR_1 0
 #define TEMP_SENSOR_2 0
 #define TEMP_SENSOR_3 0
@@ -641,7 +641,7 @@
 // Above this temperature the heater will be switched off.
 // This can protect components from overheating, but NOT from shorts and failures.
 // (Use MINTEMP for thermistor short/failure protection.)
-#define HEATER_0_MAXTEMP 275      //最高预热温度等于该值-15℃
+#define HEATER_0_MAXTEMP 365
 #define HEATER_1_MAXTEMP 275
 #define HEATER_2_MAXTEMP 275
 #define HEATER_3_MAXTEMP 275
@@ -649,7 +649,7 @@
 #define HEATER_5_MAXTEMP 275
 #define HEATER_6_MAXTEMP 275
 #define HEATER_7_MAXTEMP 275
-#define BED_MAXTEMP      125      //最高预热温度等于该值-15℃
+#define BED_MAXTEMP      150
 #define CHAMBER_MAXTEMP  60
 
 /**
