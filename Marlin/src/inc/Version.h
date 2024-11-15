16a93a8b7ba1af3bf4b7dc0f38f0c48f34177dc2
Scott Lahteine <thinkyhead@users.noreply.github.com>
2024-11-15 16:16:22 -0600

    🔖 Version 2.1.2.5

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -25,7 +25,7 @@
  * Release version. Leave the Marlin version or apply a custom scheme.
  */
 #ifndef SHORT_BUILD_VERSION
-  #define SHORT_BUILD_VERSION "2.1.2.4"
+  #define SHORT_BUILD_VERSION "2.1.2.5"
 #endif
 
 /**
@@ -42,7 +42,7 @@
  * version was tagged.
  */
 #ifndef STRING_DISTRIBUTION_DATE
-  #define STRING_DISTRIBUTION_DATE "2024-06-16"
+  #define STRING_DISTRIBUTION_DATE "2024-11-15"
 #endif
 
 /**
@@ -101,8 +101,8 @@
 #endif
 
 /**
- * Set the vendor info the serial USB interface, if changable
- * Currently only supported by DUE platform
+ * Set the vendor info the serial USB interface, if changeable.
+ * Currently only supported by DUE platform.
  */
 #ifndef USB_DEVICE_VENDOR_ID
   #define USB_DEVICE_VENDOR_ID            0x03EB /* ATMEL VID */
