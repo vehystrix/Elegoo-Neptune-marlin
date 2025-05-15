dd16a8525d0ec9bb1faed6f5c43187cb4a8f12a3
VEhystrix <VEhystrix@gmail.com>
2025-05-15 16:19:05 +0200

    Enabled XY_FREQUENCY_LIMIT

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1315,7 +1315,7 @@
  * See https://hydraraptor.blogspot.com/2010/12/frequency-limit.html
  * Use M201 F<freq> S<min%> to change limits at runtime.
  */
-//#define XY_FREQUENCY_LIMIT      10 // (Hz) Maximum frequency of small zigzag infill moves. Set with M201 F<hertz>.
+#define XY_FREQUENCY_LIMIT      10 // (Hz) Maximum frequency of small zigzag infill moves. Set with M201 F<hertz>.
 #ifdef XY_FREQUENCY_LIMIT
   #define XY_FREQUENCY_MIN_PERCENT 5 // (%) Minimum FR percentage to apply. Set with M201 S<min%>.
 #endif
