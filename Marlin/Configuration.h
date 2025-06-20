605f64f7dd1a96d4de7b1568fc7d5634f53314bf
Caleb Sexton <github@calebsexton.com>
2025-06-19 18:09:15 -0700

    Disable probe offset for bed leveling
    The default configuration of the Neptune 3 uses the nozzle as the bed probe. 
    
    NOZZLE_TO_PROBE_OFFSET causes the bed mesh to be off-center and correspondingly inaccurate.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1704,7 +1704,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET {-28.5, 22, 0 }
+//#define NOZZLE_TO_PROBE_OFFSET {-28.5, 22, 0 }
 
 // Enable and set to use a specific tool for probing. Disable to allow any tool.
 #define PROBING_TOOL 0
