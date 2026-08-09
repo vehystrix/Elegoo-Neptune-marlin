cd22a63e2f879037e3686501438af3222e8afb1e
D1SK0V3RY <73703371+D1SK0V3RY@users.noreply.github.com>
2026-08-09 12:13:15 +0300

    defined FAN_SOFT_PWM
    Reverting unnecessary changes

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -3777,7 +3777,7 @@
  * which is not as annoying as with the hardware PWM. On the other hand, if this frequency
  * is too low, you should also increment SOFT_PWM_SCALE.
  */
-//#define FAN_SOFT_PWM
+#define FAN_SOFT_PWM
 
 /**
  * Incrementing this by 1 will double the software PWM frequency, affecting heaters, and
