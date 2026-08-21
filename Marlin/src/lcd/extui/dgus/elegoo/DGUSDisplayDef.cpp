e2dabfcd76681f92bd32d11845fc601ec837d054
VEhystrix <VEhystrix@gmail.com>
2026-08-22 00:42:50 +0200

    Added ToC Fixed bug in petg temp settings

--- Marlin/src/lcd/extui/dgus/elegoo/DGUSDisplayDef.cpp
+++ Marlin/src/lcd/extui/dgus/elegoo/DGUSDisplayDef.cpp
@@ -4679,7 +4679,7 @@
             else if(recdat.data[0]==2)
             {
               petg_extrusion_temp = (petg_extrusion_temp - unit);
-              if(pla_extrusion_temp<160)
+              if(petg_extrusion_temp<160)
               {
                 petg_extrusion_temp = 160;
               }        
